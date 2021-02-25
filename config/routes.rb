78f5b768fd30a82e18a4e816348c42bc9785d2c7
KumarAmitt <amitt2015@outlook.com>
2021-02-25 15:02:29 +0530

    Add Routes and registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
+
   devise_for :users
-  resources :posts, only: [:new, :create, :index]
+  # resources :posts, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  root 'posts#index'
+
 end
+
+
