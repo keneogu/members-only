5fe0efe0c66f946f121e36e8309b48407742ab8f
KumarAmitt <amitt2015@outlook.com>
2021-02-25 14:42:42 +0530

    Add ueser model and devise gem

--- db/migrate/20210224145646_create_posts.rb
+++ db/migrate/20210224145646_create_posts.rb
@@ -1,9 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
+      t.integer :user_id
+      t.string :title
       t.text :post
 
       t.timestamps
     end
+    add_index :posts, :user_id, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_145646) do
+ActiveRecord::Schema.define(version: 2021_02_25_085754) do
 
   create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
     t.text "post"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
